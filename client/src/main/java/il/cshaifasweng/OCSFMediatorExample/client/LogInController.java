83643cfc587bfd49eddb521351665be047bc1529
asadmahajna <amahajna@salesforce.com>
2023-01-11 14:42:40 +0200

    Added login screen and controller, Still need to fix the RadioButton!

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LogInController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/LogInController.java
@@ -172,6 +172,7 @@ public class LogInController{
     private boolean validateEmail(String mail){
         String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
         return Pattern.compile(regexPattern).matcher(mail).matches();
     }
 
