bec2960839758ac0dc13694995cf0a8ae8940eec
Mohamad AbuAbied <82101799+abuabied@users.noreply.github.com>
2023-02-15 11:05:22 +0200

    Parking Lot Employee Updates
    -logout button is functional
    -mark/unmark spot as Faulty
    -selection from table or combo box
    -selected spot details synced in table and comboBox
    -buttons (save/mark spot) available only when spot is selected

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Employee_GUI_Controller.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Employee_GUI_Controller.java
@@ -1,5 +1,6 @@
 package il.cshaifasweng.OCSFMediatorExample.client;
 
+import il.cshaifasweng.LogInEntities.Employees.ParkingLotEmployee;
 import il.cshaifasweng.Message;
 import il.cshaifasweng.OCSFMediatorExample.client.Subscribers.ParkingSpotsSubscriber;
 import il.cshaifasweng.ParkingLotEntities.ParkingSpot;
@@ -16,7 +17,9 @@ import static com.sun.javafx.application.PlatformImpl.runLater;
 
 
 import javax.persistence.criteria.CriteriaBuilder;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Employee_GUI_Controller {
@@ -58,16 +61,16 @@ public class Employee_GUI_Controller {
     private ComboBox<Integer> rowLabel;
 
     @FXML
-    private Button userOptBtn;
+    private Label userNameLbl;
 
     @FXML
-    private Label errorMsg;
+    private Button faultyBtn;
 
     @FXML
     private Button markFaultyBtn;
 
     @FXML
-    private Button userOptBtn1;
+    private Button logOutBtn;
 
     @FXML
     void DirectToAvailblePark(ActionEvent event) {
@@ -82,19 +85,45 @@ public class Employee_GUI_Controller {
     @FXML
     void SaveSpot(ActionEvent event) {
         if (Table.getSelectionModel().getSelectedItem() != null) {
-//            errorMsg.setVisible(false);
-            Table.getSelectionModel().getSelectedItem().setOccupied(true);
+            Table.getSelectionModel().getSelectedItem().setSaved(true);
             System.out.println(Table.getSelectionModel().getSelectedItem());
-            Message message = new Message("#SetParkingSpots");
-            message.setObject(Table.getSelectionModel().getSelectedItem());
-            try {
-                SimpleClient.getClient().sendToServer(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            //errorMsg.setVisible(true);
+            updateSpot();
+            initControlsState();
         }
+    }
+
+    @FXML
+    void markUnmarkFaulty(ActionEvent event) {
+        if (Table.getSelectionModel().getSelectedItem() != null) {
+            Table.getSelectionModel().getSelectedItem().setFaulty(!Table.getSelectionModel().getSelectedItem().isFaulty());
+            System.out.println(Table.getSelectionModel().getSelectedItem());
+            updateSpot();
+            initControlsState();
+        }
+    }
+
+    private void updateSpot() {
+        Message message = new Message("#SetParkingSpots");
+        message.setObject(Table.getSelectionModel().getSelectedItem());
+        try {
+            SimpleClient.getClient().sendToServer(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getSpotStatus(ParkingSpot ps) {
+        if (ps.isSaved())
+            return "Saved";
+        if (ps.isFaulty())
+            return "Faulty";
+        if (ps.isOccupied())
+            return "Occupied";
+        return "Free";
+    }
+
+    private boolean isAvailable(ParkingSpot ps) {
+        return !(ps.isSaved() || ps.isOccupied() || ps.isFaulty());
     }
 
     @FXML
@@ -110,6 +139,7 @@ public class Employee_GUI_Controller {
 
     @FXML
     void initializeBtn(ActionEvent event) {
+        initControlsState();
         Message message = new Message("#intializeParkingLot");
         try {
             SimpleClient.getClient().sendToServer(message);
@@ -120,15 +150,15 @@ public class Employee_GUI_Controller {
 
     @Subscribe
     public void GetParkingSpotsFromServer(ParkingSpotsSubscriber event) {
-
         System.out.println("Got response from server");
         pSpots = (List<ParkingSpot>) event.getMessage().getObject();
+        initComboBox();
         pSpots.forEach(ps -> System.out.println(isAvailable(ps)));
         row.setCellValueFactory(new PropertyValueFactory<>("row"));
         floor.setCellValueFactory(new PropertyValueFactory<>("floor"));
         depth.setCellValueFactory(new PropertyValueFactory<>("depth"));
         available.setCellValueFactory(data -> new SimpleObjectProperty<>(isAvailable(data.getValue())));
-        //statusColumn.setCellValueFactory(data -> new SimpleObjectProperty<>(getSpotStatus(data.getValue())));
+        statusColumn.setCellValueFactory(data -> new SimpleObjectProperty<>(getSpotStatus(data.getValue())));
         refreshTable();
     }
 
@@ -141,45 +171,80 @@ public class Employee_GUI_Controller {
         });
     }
 
-    private String getSpotStatus(ParkingSpot ps) {
-        if (ps.free())
-            return "Free";
-        if (ps.isFaulty())
-            return "Faulty";
-        if (ps.isOccupied())
-            return "Occupied";
-        return "Saved";
-    }
-
-    private boolean isAvailable(ParkingSpot ps){
-        return !(ps.isSaved() || ps.isOccupied() || ps.isFaulty());
-    }
-
-    @FXML
-    void markSpotAsFaulty(ActionEvent event) {
-        if (Table.getSelectionModel().getSelectedItem() != null) {
-//            errorMsg.setVisible(false);
-            Table.getSelectionModel().getSelectedItem().setFaulty(true);
-            System.out.println(Table.getSelectionModel().getSelectedItem());
-            Message message = new Message("#SetParkingSpots");
-            message.setObject(Table.getSelectionModel().getSelectedItem());
-            try {
-                SimpleClient.getClient().sendToServer(message);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            //errorMsg.setVisible(true);
-        }
-    }
-
-
     @FXML
     void initialize() throws Exception {
         EventBus.getDefault().register(this);
-        //errorMsg.setVisible(false);
         Message message = new Message("#GetParkingSpots");
         SimpleClient.getClient().sendToServer(message);
+        String name = ((ParkingLotEmployee)SimpleChatClient.getUser()).getFirstName();
+        userNameLbl.setText("Hello, " + name);
+        faultyBtn.setDisable(true);
+        SaveSpot.setDisable(true);
+    }
+
+    private void initComboBox() {
+        pSpots.forEach(spot -> {
+            if (rowLabel.getItems().lastIndexOf(spot.getRow()) == -1)
+                rowLabel.getItems().add(spot.getRow(), spot.getRow());
+            if (floorLabel.getItems().lastIndexOf(spot.getFloor()) == -1)
+                floorLabel.getItems().add(spot.getFloor(), spot.getFloor());
+            if (depthLabel.getItems().lastIndexOf(spot.getDepth()) == -1)
+                depthLabel.getItems().add(spot.getDepth(), spot.getDepth());
+        });
+
+    }
+
+    @FXML
+    public void spotSelectedTable(javafx.scene.input.MouseEvent mouseEvent) {
+        ParkingSpot ps = Table.getSelectionModel().getSelectedItem();
+        rowLabel.getSelectionModel().select(ps.getRow());
+        floorLabel.getSelectionModel().select(ps.getFloor());
+        depthLabel.getSelectionModel().select(ps.getDepth());
+        faultyBtn.setDisable(false);
+        SaveSpot.setDisable(false);
+    }
+
+    @FXML
+    void spotSelectedComboBox(ActionEvent event) {
+        if (rowLabel.getValue() != null && floorLabel.getValue() != null
+                && depthLabel.getValue() != null) {
+            faultyBtn.setDisable(false);
+            SaveSpot.setDisable(false);
+            Table.getSelectionModel().select(Table.getItems().get(findSpotIndexInTable()));
+        }
+    }
+
+    private int findSpotIndexInTable() {
+        int i = 0;
+        List<ParkingSpot> parkingSpots = Table.getItems();
+        for (ParkingSpot sp :
+                parkingSpots) {
+            if (rowLabel.getSelectionModel().getSelectedItem() == sp.getRow() &&
+                    floorLabel.getSelectionModel().getSelectedItem() == sp.getFloor() &&
+                    depthLabel.getSelectionModel().getSelectedItem() == sp.getDepth()) {
+                sp.toString();
+                return i;
+            }
+            i++;
+        }
+        return 0;
+    }
+
+    @FXML
+    void logOutUser(ActionEvent event) {
+        try {
+            SimpleChatClient.setRoot("logInScreen");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initControlsState(){
+        faultyBtn.setDisable(true);
+        SaveSpot.setDisable(true);
+        rowLabel.getSelectionModel().clearSelection();
+        depthLabel.getSelectionModel().clearSelection();
+        floorLabel.getSelectionModel().clearSelection();
     }
 
 }
